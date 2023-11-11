fb404732eaab3d64dca1ece530227164c4b2dc6a
luohao <1223048059@qq.com>
2023-11-11 17:04:32 +0800

    feat: add dashboard template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,34 @@
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
+import { cn } from "~/lib/utils";
+import { ThemeProvider } from "~/components/theme-provider";
 
 import "~/styles/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        inter.variable,
+      )}
+    >
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
