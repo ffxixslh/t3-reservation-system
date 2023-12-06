1b600ea8649337a7c743515a8983c212db6876e9
luohao <1223048059@qq.com>
2023-12-06 21:22:41 +0800

    chore: change another method to check false value

--- src/app/dashboard/(dashboard)/[hospitalId]/(routes)/departments/components/columns.tsx
+++ src/app/dashboard/(dashboard)/[hospitalId]/(routes)/departments/components/columns.tsx
@@ -21,7 +21,7 @@ export const columns: ColumnDef<TDepartment>[] = [
     header: "描述",
     cell: ({ row }) => (
       <div className="flex items-center gap-x-2">
-        {row.original?.description === null
+        {[null, ""].includes(row.original?.description)
           ? "无"
           : row.original?.description}
       </div>
