beecdc47ea2244d82dbad6fe9b26167593ce8264
luohao <1223048059@qq.com>
2023-12-14 00:04:15 +0800

    ui: added description item

---
+++ src/components/ui/description-item.tsx
@@ -0,0 +1,30 @@
+import { cn } from "~/lib/utils";
+import { Label } from "./label";
+
+interface DescriptionItemProps {
+  title: string;
+  content?: string;
+  children?: React.ReactNode;
+  className?: string;
+}
+
+const DescriptionItem: React.FC<DescriptionItemProps> = ({
+  title,
+  content,
+  children,
+  className,
+}) => {
+  return (
+    <div className={cn("flex flex-col gap-2", className)}>
+      <Label className="text-muted-foreground">
+        {title}
+      </Label>
+      {content && (
+        <div className="font-200 text-lg">{content}</div>
+      )}
+      {children}
+    </div>
+  );
+};
+
+export default DescriptionItem;
