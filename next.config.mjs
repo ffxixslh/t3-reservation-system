d009991d44df316c0e2757e863645f79da05f2f6
luohao <1223048059@qq.com>
2024-03-09 20:30:08 +0800

    chore: updated config

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,6 @@
 await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
-const config = {
-  images: {
-    domains: ["res.cloudinary.com"]
-  }
-};
+const config = {};
 
 export default config;
